aa78350e4bbf3f7d3d6ae9312f04e15bd02c7454
AydanKazimzada <aydankazimzadeh@gmail>
2024-08-31 20:24:28 +0400

    create web app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -108,7 +108,7 @@ class ModelTrainer:
             predicted = best_model.predict(X_test)
 
             r2 = r2_score(y_test,predicted)
-            return r2
+            return best_model_name, r2
 
         except Exception as e:
             raise CustomException(e,sys)
